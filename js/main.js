0348cf22482e3639958a6f17e256547b68c5f569
Samuel Scozzari <s_scozzari@libero.it>
2021-12-09 15:43:56 +0100

    added comments about buttonMenu, delMenu events

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ let buttonMenu = document.querySelector(".fa-bars");
 let delMenu = document.querySelector(".fa-times");
 let hamMenu = document.querySelector(".hamburger-menu");
 
+/* 
+Al click sull'icona del menù (visualizzazione smartphone & tablet), la funzione sottostante 
+aggiungerà la classe 'active' vicino a quella chiamata 'hamburger-menu' all'interno del file html. 
+La classe active caratterizzata dalla proprietà 'display: block;' permetterà la visualizzazione del menù a scomparsa
+*/
 buttonMenu.addEventListener("click", function () {
   hamMenu.classList.add("active");
 });
 
+/* 
+Al click sull'icona rappresentante una X, visualizzabile soltanto dopo aver aperto il menù a scomparsa (visualizzazione smartphone & tablet), 
+la funzione sottostante causerà la chiusura del suddetto menu. 
+*/
 delMenu.addEventListener("click", function () {
   hamMenu.classList.remove("active");
 });
