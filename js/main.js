df2535b9ea1845524bc3e2136a4a373dd70144e0
Samuel Scozzari <s_scozzari@libero.it>
2021-12-09 15:22:12 +0100

    added buttonMenu event listener

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 let buttonMenu = document.querySelector(".fa-bars");
 let hamMenu = document.querySelector(".hamburger-menu");
+
+buttonMenu.addEventListener("click", function () {
+  hamMenu.classList.add("active");
+});
