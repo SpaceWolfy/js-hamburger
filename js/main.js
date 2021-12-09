07fbb96caf532bd587957bd21038d2beaf9cf962
Samuel Scozzari <s_scozzari@libero.it>
2021-12-09 15:30:29 +0100

    added delMenu let, added delMenu event listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let buttonMenu = document.querySelector(".fa-bars");
+let delMenu = document.querySelector(".fa-times");
 let hamMenu = document.querySelector(".hamburger-menu");
 
 buttonMenu.addEventListener("click", function () {
   hamMenu.classList.add("active");
+});
+
+delMenu.addEventListener("click", function () {
+  hamMenu.classList.remove("active");
 });
